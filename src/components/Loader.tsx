59d065e4e96cbdc2e97e965d6e29cae243267e55
Muhammad Usama <liontiger49@yahoo.com>
2024-05-29 18:54:30 +0500

    better loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,9 @@ const Loader = () => {
   return (
     <Html>
       <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center'>
-        <div className='w-[10vw] h-[10vw] rounded-full'>Loading...</div>
+        <div className='w-[10vw] h-[10vw] rounded-full'>
+          <span className='loader'></span>
+        </div>
       </div>
     </Html>
   );
